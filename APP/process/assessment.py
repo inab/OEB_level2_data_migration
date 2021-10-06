8a56768218d01a8333ac664c43efd05b8e76ddc2
txellfe <meritxell.ferret@bsc.es>
2021-10-06 17:34:53 +0200

    fix syntax

--- APP/process/assessment.py
+++ APP/process/assessment.py
@@ -128,8 +128,11 @@ class Assessment():
                 if dataset['community_id'] in metric['orig_id']:
                     if dataset["metrics"]["metric_id"].upper() in metric['orig_id'].upper():
                          metric_id = metric["_id"]
-                    elif dataset["metrics"]["metric_id"].upper() in metric['_metadata']['level_2:metric_id'].upper():
-                    	  metric_id = metric["_id"]
+                    if metric['_metadata']:
+                    	  if dataset["metrics"]["metric_id"].upper() in metric['_metadata']['level_2:metric_id'].upper():
+                    	     metric_id = metric["_id"]
+                    
+                    
              
 
             try:
