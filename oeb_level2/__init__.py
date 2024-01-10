d1b457954fc6a9be7622160e6e0865b0a59bc53f
José Mª Fernández <jose.m.fernandez@bsc.es>
2024-01-10 01:39:20 +0100

    Version bump to 2.1.0

--- oeb_level2/__init__.py
+++ oeb_level2/__init__.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0-only
 # Copyright (C) 2020 Barcelona Supercomputing Center, Javier Garrayo Ventas
 # Copyright (C) 2020-2022 Barcelona Supercomputing Center, Meritxell Ferret
-# Copyright (C) 2020-2023 Barcelona Supercomputing Center, José M. Fernández
+# Copyright (C) 2020-2024 Barcelona Supercomputing Center, José M. Fernández
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 
 name = "OEB_level2_data_migration"
 __author__ = "Javier Garrayo Ventas <https://orcid.org/0000-0003-0015-1573>, Meritxell Ferret <https://orcid.org/0000-0003-3995-7611>, José M. Fernández <https://orcid.org/0000-0002-4806-5140>"
-__copyright__ = "© 2020-2023 Barcelona Supercomputing Center (BSC), ES"
+__copyright__ = "© 2020-2024 Barcelona Supercomputing Center (BSC), ES"
 __license__ = "GPLv3"
 
-__version__ = version = "2.0.8"
+__version__ = version = "2.1.0"
